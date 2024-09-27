9f491f2d32a3bfe204760498ac3a162f59186e01
nzadap <nzadap@redhat.com>
2024-09-27 10:45:17 +0530

    First

--- src/test/java/utils/PlaywrightListeners.java
+++ src/test/java/utils/PlaywrightListeners.java
@@ -22,7 +22,7 @@ public class PlaywrightListeners implements ITestListener {
        try {
            Allure.addAttachment("Screenshot", new ByteArrayInputStream(Files.readAllBytes(screenshotPath)));
        } catch (IOException e) {
-           throw new RuntimeException(e);
+           throw new RuntimeException(e );
        }
     }
 }
